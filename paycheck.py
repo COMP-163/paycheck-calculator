1d9f42d9326ba10812341568584e07e76c10f6ff
madison-ann-jones <madisonjones084@icloud.com>
2026-09-17 11:50:15 -0400

    Add name and date to paycheck.py header
    Updated header comments with name and date.

--- paycheck.py
+++ paycheck.py
@@ -1,21 +1,24 @@
-# Name:
-# Date:
+# Name: Madison Jones
+# Date: 9/18/26
 # Course: COMP 163
 # Project 1: Paycheck Calculator
 
 # Put your name after "# Name:" above. The grader checks that it is filled in.
 
 # Read four values from the user, in this order:
-#   1. Employee name
-#   2. Hours worked
-#   3. Hourly rate
-#   4. Tax rate, as a percent (for example, 10 means 10%)
+name = input('Enter Employee Name: ')
+hrs_work = float(input('Enter Hours Worked: '))
+hr_rate = float(input('Enter Hourly Rate: '))
+tax_rate = float(input('Enter Tax Rate: '))
+
 #
 # Hours worked and the hourly rate can have a fraction in them, like 37.5
 # hours or 10.25 hours. Use float() for all three numbers, not int().
 # int("37.5") crashes.
 #
 # Then calculate:
+gross_pay = hrs_wrk * hr_rate
+tax_held = 
 #   gross pay     = hours worked * hourly rate
 #   tax withheld  = gross pay * (tax rate / 100)
 #   net pay       = gross pay - tax withheld
